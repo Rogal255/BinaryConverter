2c9de79128ffba0a861270893d24a0ffe9350e8f
Piotr Rogalski <progalski89@gmail.com>
2022-08-13 18:54:58 +0200

    Further work on xtd layout

--- Frontends/xtd/Form.hpp
+++ Frontends/xtd/Form.hpp
@@ -7,7 +7,12 @@ public:
 
 private:
     xtd::forms::vertical_layout_panel scene_;
+    xtd::forms::horizontal_layout_panel resultsBoxPanel_;
 
     xtd::forms::text_box inputBox_;
     xtd::forms::group_box resultsBox_;
+    xtd::forms::label resultLabel1_;
+    xtd::forms::label resultLabel2_;
+    xtd::forms::text_box resultBox1_;
+    xtd::forms::text_box resultBox2_;
 };
