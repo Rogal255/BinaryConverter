381f36bf9232906ddd9daf98ab00d31f1f2b4fd4
Piotr Rogalski <progalski89@gmail.com>
2022-08-19 23:01:20 +0200

    Added base input panel and buttons for saving and clearing in xtd

--- Frontends/xtd/Form.cpp
+++ Frontends/xtd/Form.cpp
@@ -10,9 +10,10 @@ Form::Form() {
 
     scene_.dock(xtd::forms::dock_style::fill);
     scene_.padding(xtd::forms::padding(10));
-    scene_.controls().push_back_range({inputBox_, resultsBox_});
+    scene_.controls().push_back_range({inputBox_, resultsBox_, optionsPanel_});
     scene_.control_layout_style(inputBox_, {75, xtd::forms::size_type::absolute, true});
     scene_.control_layout_style(resultsBox_, {70, xtd::forms::size_type::absolute, true});
+    scene_.control_layout_style(optionsPanel_, {80, xtd::forms::size_type::absolute, true});
 
     inputBox_.text("Lorem ipsum...");
     inputBox_.font(xtd::drawing::font(inputBox_.font(), 30));
@@ -30,4 +31,27 @@ Form::Form() {
     resultLabel2_.text("HEX");
     resultLabel1_.text_align(xtd::forms::content_alignment::middle_right);
     resultLabel2_.text_align(xtd::forms::content_alignment::middle_right);
-};
+    optionsPanel_.controls().push_back_range({inputBaseBox_,optionsSpacerPanel_, optionsButtonsPanel_});
+    optionsPanel_.control_layout_style(inputBaseBox_, {0.45f, xtd::forms::size_type::percent, true});
+    optionsPanel_.control_layout_style(optionsSpacerPanel_, {20, xtd::forms::size_type::absolute, true});
+    optionsPanel_.control_layout_style(optionsButtonsPanel_, {xtd::forms::size_type::auto_size, true});
+    inputBaseBox_.controls().push_back(inputBaseBoxPanel_);
+    inputBaseBox_.text("Input base");
+    inputBaseBox_.controls().push_back(radioButtonsPanel_);
+    radioButtonsPanel_.dock(xtd::forms::dock_style::fill);
+    radioButtonsPanel_.controls().push_back_range({decRadioButton_, binRadioButton_, hexRadioButton_});
+    radioButtonsPanel_.padding(xtd::forms::padding(0, 0, 0, 10));
+    inputBaseBoxPanel_.dock(xtd::forms::dock_style::fill);
+    decRadioButton_.text("Dec");
+    binRadioButton_.text("Bin");
+    hexRadioButton_.text("Hex");
+    radioButtonsPanel_.control_layout_style(decRadioButton_, {xtd::forms::size_type::auto_size, true});
+    radioButtonsPanel_.control_layout_style(binRadioButton_, {xtd::forms::size_type::auto_size, true});
+    radioButtonsPanel_.control_layout_style(hexRadioButton_, {xtd::forms::size_type::auto_size, true});
+    optionsButtonsPanel_.padding(xtd::forms::padding(0, 5, 0, 0));
+    optionsButtonsPanel_.controls().push_back_range({saveButton_, clearButton_});
+    saveButton_.text("Save");
+    clearButton_.text("Clear");
+    optionsButtonsPanel_.control_layout_style(saveButton_, {xtd::forms::size_type::auto_size, true});
+    optionsButtonsPanel_.control_layout_style(clearButton_, {xtd::forms::size_type::auto_size, true});
+}
--- Frontends/xtd/Form.hpp
+++ Frontends/xtd/Form.hpp
@@ -8,11 +8,23 @@ public:
 private:
     xtd::forms::vertical_layout_panel scene_;
     xtd::forms::horizontal_layout_panel resultsBoxPanel_;
+    xtd::forms::horizontal_layout_panel optionsPanel_;
+    xtd::forms::horizontal_layout_panel inputBaseBoxPanel_;
+    xtd::forms::vertical_layout_panel optionsButtonsPanel_;
+    xtd::forms::panel optionsSpacerPanel_;
 
     xtd::forms::text_box inputBox_;
     xtd::forms::group_box resultsBox_;
+    xtd::forms::group_box inputBaseBox_;
+    xtd::forms::button saveButton_;
+    xtd::forms::button clearButton_;
     xtd::forms::label resultLabel1_;
     xtd::forms::label resultLabel2_;
     xtd::forms::text_box resultBox1_;
     xtd::forms::text_box resultBox2_;
+
+    xtd::forms::radio_button decRadioButton_;
+    xtd::forms::radio_button binRadioButton_;
+    xtd::forms::radio_button hexRadioButton_;
+    xtd::forms::horizontal_layout_panel radioButtonsPanel_;
 };
